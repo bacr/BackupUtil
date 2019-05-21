1140edcaedb64e5b9db6d060e718e33f93d74ecc
Vassily Ivanov <vivanov@aeb-it.ru>
2019-05-21 14:54:17 +0900

    Add project files.

---
+++ BackupUtil/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using BackupUtil.Infrastructure;
+
+namespace BackupUtil
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            NLog.LogManager.LoadConfiguration("nlog.config");
+            var builder = new HostBuilder()
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.AddJsonFile("hostsettings.json", optional: true);
+                    configHost.AddEnvironmentVariables();
+                })
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: true);
+                    config.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json",
+                        optional: true);
+                    config.AddJsonFile("appsettings.local.json", optional: true);
+                    config.AddEnvironmentVariables();
+
+                    if (args != null)
+                    {
+                        config.AddCommandLine(args);
+                    }
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddOptions();
+                })
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureContainer<ContainerBuilder>((hostContext, container) =>
+                {
+                    AutofacConfig.ConfigureAutofac(container, hostContext.Configuration);
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddNLog();
+                });
+
+            await builder.RunConsoleAsync();
+
+            NLog.LogManager.Shutdown();
+        }
+    }
+}
