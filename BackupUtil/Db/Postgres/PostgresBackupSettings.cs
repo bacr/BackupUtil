6eefd76a05fdf3ecf06d2bd0a4790982aa48e9db
Vassily Ivanov <vivanov@sharplogic.ru>
2022-10-02 20:22:48 +0900

    #4 Add PostgresBackup (#7)

---
+++ BackupUtil/Db/Postgres/PostgresBackupSettings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupUtil.Db.Postgres
+{
+    public class PostgresBackupSettings
+    {
+        public string? Database { get; set; }
+
+        public string[]? Databases { get; set; }
+
+        public string? Host { get; set; }
+
+        public int Port { get; set; } = 5432;
+
+        public string? User { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? BackupPath { get; set; }
+
+        public string[]? GetDatabases()
+        {
+            if (Databases != null)
+            {
+                return Databases;
+            }
+            if (Database != null)
+            {
+                return new[] { Database };
+            }
+            return null;
+        }
+    }
+}
